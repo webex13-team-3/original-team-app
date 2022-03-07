b1f6a51b45ba10229edcbcb7bce70367085ada58
junseinagao <junsei.nagao@gmail.com>
2022-03-07 16:57:16 +0900

    ant designを導入して、ページを整理しました。

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import IndexMakeView from "../views/IndexMakeView"
+import DetailMakeView from "../views/DetailMakeView"
+import PostMakeView from "../views/PostMakeView"
 
 const routes = [
   {
@@ -8,13 +11,19 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/makes",
+    name: "index-makes",
+    component: IndexMakeView,
+  },
+  {
+    path: "/makes/detail", // TODO `detail` の部分を firebase の id に 動的に置き換える
+    name: "detail-make",
+    component: DetailMakeView,
+  },
+  {
+    path: "/post",
+    name: "post-make",
+    component: PostMakeView,
   },
 ]
 
