96443b49f4b36f6bd84ee994f85232ff93ba9840
mikuA <akmt920@gmail.com>
2022-03-10 13:03:22 +0900

    IndexMakeviewの編集

--- src/views/IndexMakeView.vue
+++ src/views/IndexMakeView.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>一覧ページ</h1>
-  <div>
-    <p v-for="make in makes" :key="make.id">
-      {{ make.text }}
-      {{ make.sample.shitajiName }}
+  <div v-for="make in makes" :key="make.id">
+    <p>
+      {{ make.shitajiName }}
+    </p>
+    <p>
+      <img v-bind:src="make.shitajiImage" alt="下地のイメージ" width="200" />
     </p>
   </div>
 </template>
 
 <script>
-import { collection, addDoc, getDocs } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore"
 import { db } from "@/firebase"
 
 export default {
@@ -19,21 +21,16 @@ export default {
     }
   },
   methods: {
-    postTweet() {
-      addDoc(collection(db, "makes"), {
-        text: "こんにちは、ツイートの本文です。",
-      })
-    },
-  },
-  created() {
-    getDocs(collection(db, "makes")).then((snapshot) => {
-      snapshot.forEach((doc) => {
-        this.makes.push({
-          id: doc.id,
-          ...doc.data(),
+    created() {
+      getDocs(collection(db, "makes")).then((snapshot) => {
+        snapshot.forEach((doc) => {
+          this.makes.push({
+            id: doc.id,
+            ...doc.data(),
+          })
         })
       })
-    })
+    },
   },
 }
 </script>
