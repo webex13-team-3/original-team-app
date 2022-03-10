a0c260db7a0d52b5d3c81da19ab6d11a6ad01296
mikuA <akmt920@gmail.com>
2022-03-10 10:04:21 +0900

    IndexMakeviewの途中経過

--- src/views/IndexMakeView.vue
+++ src/views/IndexMakeView.vue
@@ -1,3 +1,39 @@
 <template>
   <h1>一覧ページ</h1>
+  <div>
+    <p v-for="make in makes" :key="make.id">
+      {{ make.text }}
+      {{ make.sample.shitajiName }}
+    </p>
+  </div>
 </template>
+
+<script>
+import { collection, addDoc, getDocs } from "firebase/firestore"
+import { db } from "@/firebase"
+
+export default {
+  data() {
+    return {
+      makes: [],
+    }
+  },
+  methods: {
+    postTweet() {
+      addDoc(collection(db, "makes"), {
+        text: "こんにちは、ツイートの本文です。",
+      })
+    },
+  },
+  created() {
+    getDocs(collection(db, "makes")).then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.makes.push({
+          id: doc.id,
+          ...doc.data(),
+        })
+      })
+    })
+  },
+}
+</script>
