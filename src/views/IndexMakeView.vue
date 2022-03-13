fa1fac021242eae98b6410c66369ee0fafc3254b
mikuA <akmt920@gmail.com>
2022-03-13 10:34:39 +0900

    IndexMakevieのタグ付け

--- src/views/IndexMakeView.vue
+++ src/views/IndexMakeView.vue
@@ -1,12 +1,35 @@
 <template>
   <h1>一覧ページ</h1>
-  <div v-for="make in makes" :key="make.id">
-    <p>
-      {{ make.shitajiName }}
-    </p>
-    <p>
-      <img v-bind:src="make.shitajiImage" alt="下地のイメージ" width="200" />
-    </p>
+  <div class="container">
+    <div v-for="make in makes" :key="make.id" class="container-box">
+      <p>
+        <img v-bind:src="make.shitajiImage" alt="下地のイメージ" width="200" />
+      </p>
+      <div class="tag-box">
+        <p
+          v-if="
+            make.shitajiName ||
+            make.concealerName ||
+            make.shadingName ||
+            make.highlightName
+          "
+          class="tag-name"
+        >
+          ベースメイク
+        </p>
+        <p v-if="make.powderName || make.cheekName" class="tag-name">
+          パウダー
+        </p>
+        <ListTag
+          :name="hasEyeMakes(make)"
+          tagName="アイメイク"
+          class="tag-name"
+        />
+
+        <p v-if="make.lipName" class="tag-name">リップ</p>
+        <p v-if="make.otherName" class="tag-name">その他</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,17 +43,52 @@ export default {
       makes: [],
     }
   },
-  methods: {
-    created() {
-      getDocs(collection(db, "makes")).then((snapshot) => {
-        snapshot.forEach((doc) => {
-          this.makes.push({
-            id: doc.id,
-            ...doc.data(),
-          })
+  computed() {
+    hasEyeMakes() {
+      return function (make) {
+      if (make.eyeShadowName || make.mascaraName || make.eyeblowName) return true
+      return false
+      }
+    }
+  },
+  methods: {},
+  created() {
+    getDocs(collection(db, "makes")).then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.makes.push({
+          id: doc.id,
+          ...doc.data(),
         })
       })
-    },
+    })
   },
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  text-align: center;
+}
+.container-box {
+  width: 30%;
+  margin: 20px;
+}
+.tag-box {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-left: 35px;
+}
+.tag-name {
+  border: 3px solid;
+  border-color: #eeb4a8;
+  width: 25%;
+  margin: 3px;
+  padding: 4px;
+  border-radius: 10%;
+}
+</style>
