60803b2571e1eb3284f5bbb2a827b8c9b89bc51a
fukushima_satsuki <fukushima.satsuki@kindai.ac.jp>
2022-03-16 12:26:00 +0900

    投稿機能を進めました

--- src/views/PostMakeView.vue
+++ src/views/PostMakeView.vue
@@ -3,12 +3,20 @@
     <h1>投稿ページ</h1>
   </div>
   <section>
-    <input type="text" v-model="inputValue" />
-    <button v-on:click="postTweet" class="form__submit-button">投稿</button>
+    <input type="text" v-model="inputValueName" />
+    <br />
+    <input type="text" v-model="inputValueComment" />
+    <br />
+    <input type="text" v-model="inputValueImage" />
+    <button v-on:click="postMake" class="form__submit-button">投稿</button>
   </section>
   <div>
-    <p v-for="tweet in tweets" :key="tweet.id">
-      {{ inputValue }}
+    <p v-for="make in makes" :key="make.id">
+      {{ make.shitajiName }}
+      <br />
+      {{ make.shitajiComment }}
+      <br />
+      {{ make.shitajiImage }}
     </p>
   </div>
 </template>
@@ -20,23 +28,20 @@ import { db } from "../firebase"
 export default {
   data() {
     return {
-      tweets: [
-        {
-          id: "",
-          inputValue: "",
-        },
-      ],
+      makes: [{}],
     }
   },
   methods: {
-    postTweet() {
-      const tweet = {
-        inputValue: "",
+    postMake() {
+      const make = {
+        shitajiName: this.inputValueName,
+        shitajiComment: this.inputValueComment,
+        shitajiImage: this.inputValueImage,
       }
-      addDoc(collection(db, "tweets"), tweet).then((ref) => {
-        this.tweets.push({
+      addDoc(collection(db, "makes"), make).then((ref) => {
+        this.makes.push({
           id: ref.id,
-          ...tweet,
+          ...make,
         })
       })
     },
