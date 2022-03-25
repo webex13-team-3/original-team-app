62539d17584afa23ce1905ccd78832ec7c0bce60
mikuA <akmt920@gmail.com>
2022-03-25 22:59:59 +0900

    topページ作りました

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/post">NewPost</router-link>
+    <div class="nav-right">
+      <p><router-link to="/">Home</router-link></p>
+    </div>
+    <div class="nav-left">
+      <router-link to="/makes" class="index">組み合わせ一覧</router-link>
+      <router-link to="/post" class="new">新新規投稿</router-link>
+    </div>
   </nav>
-
   <router-view />
 </template>
 
 <script></script>
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "游明朝";
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,15 +27,30 @@
 }
 
 nav {
-  padding: 30px;
+  width: 100%;
+  position: absolute;
+  padding-top: 15px;
+  background-color: rgba(247, 247, 247, 0.479);
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #110300;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #110300;
+}
+.nav-left {
+  width: 20%;
+}
+.nav-right {
+  width: 20%;
+}
+.index {
+  margin-right: 30px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,53 @@
 <template>
-  <div class="home">
-    <heart-outlined
-      v-bind:style="{ fontSize: '50px', color: 'pink' }"
-      v-bind:spin="true"
-    />
+  <div class="aa">
+    <h1 class="title">
+      Be More Beautiful<br />
+      Be More Yourself...<br />
+    </h1>
+
+    <router-link :to="'/makes'" class="link">すべての投稿をみる</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { HeartOutlined } from "@ant-design/icons-vue"
+
 export default {
   name: "HomeView",
-  components: {
-    HeartOutlined,
-  },
 }
 </script>
+
+<style scoped>
+.aa {
+  background-image: url(@/images/makes.jpg);
+  background-size: cover;
+  height: 100vh;
+}
+
+.title {
+  font-size: 32px;
+  position: absolute;
+  right: 10rem;
+  top: 22rem;
+  text-align: left;
+  font-weight: bold;
+  color: white;
+  text-shadow: 1px 2px 3px #494949;
+  font-family: "游明朝";
+}
+.link {
+  position: absolute;
+  top: 29rem;
+  right: 10rem;
+  font-size: 18px;
+  background-color: rgba(255, 255, 255, 0.493);
+  padding: 1rem 4rem;
+  border-radius: 15px;
+  font-weight: bold;
+  color: #7e7878;
+}
+.link:hover {
+  color: #110e0e;
+  background-color: rgba(255, 255, 255, 0.623);
+}
+</style>
